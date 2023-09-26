91d9ab4e79a820a9d2a61c5c9a2b64b275f6f7db
litonp <litondas7121@gmail.com>
2023-09-27 01:44:00 +0600

    maths

---
+++ math.js
@@ -0,0 +1,24 @@
+const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+
+if (gap < 5) {
+    // console.log('you guys can be friends');
+}
+else {
+    // console.log('you guys stay apart');
+}
+
+// ---------
+const number = 2.4998;
+const fullNumber = Math.round(number)
+// console.log(fullNumber);
+const result2 = Math.ceil(number)
+// console.log(result2);
+const result3 = Math.floor(number)
+console.log(result3);
