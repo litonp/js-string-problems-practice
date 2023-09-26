16e340a2ee2950c67e8eca58123386f445e79d5e
litonp <litondas7121@gmail.com>
2023-09-26 22:40:49 +0600

    main

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink';
+const usrtInput = 'blackPinK';
+console.log(userName.toLowerCase())
+console.log(userName.toUpperCase())
+if (userName.toLowerCase() == usrtInput.toLowerCase()) {
+    console.log('valid user');
+}
+else {
+    console.log('invalid user');
+}
