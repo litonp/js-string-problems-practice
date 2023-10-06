a71116078e3002af9f3c2af778d843af41d8f73a
litonp <litondas7121@gmail.com>
2023-10-06 15:34:40 +0600

    mathe

---
+++ max3.js
@@ -0,0 +1,26 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jim > chinku) {
+    console.log('Jim will get the cake');
+}
+else if (dela > jim && dela > chinku) {
+    console.log('Dela you will get the cake');
+}
+else {
+    console.log('Chinku will get the cake');
+}
+
+Math.max(84, 97, 99);
+console.log(Math.max(84, 97, 99));
+
+const array1 = [84, 97, 109];
+console.log(Math.max(...array1));
+
+Math.min(84, 97, 99);
+console.log(Math.min(84, 97, 99));
+
+const array2 = [83, 97, 109];
+console.log(Math.min(...array2));
+
